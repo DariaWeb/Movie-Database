1f2a5a9d8f9733d74f8e6e902ad4369799e40a9e
DariaWeb <voloshko.da@gmail.com>
2018-06-04 19:18:50 +1200

    movie database

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+
+import MoviesList from "./MoviesList";
+import MovieDetail from "./MovieDetail";
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <h1 className="App-title">Event Cinema</h1>
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
